b1a8d84d75306aba63c148f2a6888b203b11b607
Himanshu Malviya <cs20b1097@iiitdm.ac.in>
2022-04-15 15:50:01 +0530

    gjgfj

---
+++ src/routes/api/temp.js
@@ -0,0 +1,5 @@
+
+/** @type {import('@sveltejs/kit').RequestHandler} */
+export async function get() {
+    return { body: {name: 'urutrutt'}}
+}
--- src/routes/hello.svelte
+++ src/routes/hello.svelte
@@ -1,2 +1,23 @@
+<script context="module">
+    /** @type {import('@sveltejs/kit').Load}*/
+    export const load = async ({fetch}) => {
+        const req = await fetch('/api/temp');
+        const data = await req.json();
+
+        return {
+            props: {
+                hello: data
+            }
+        }
+    }
+</script>
+
+<script>
+    export let hello;
+
+    console.log(hello)
+</script>
+
+
 <h1>Hello to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
